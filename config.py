cc722196837fc56168735068d64d9d7340cc8938
贺春阳 <hechunyang.yy@gmail.com>
2017-04-20 16:59:07 +0800

    Update config.py
    新增注释

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-SAVE_PATH = "E://program_test_dir/json/"
+#文件持久化目录
+SAVE_PATH = "E://owndir/json/"
+#开始地址 以后预计加入从文件或数据库获得地址
 START_LINK = "http://www.cankaoxiaoxi.com"
+#开关
 START_SWITCH = "ON"
+#已抓取链接
 usedlinks = set()
+#全部链接
 fulllinks = set()
